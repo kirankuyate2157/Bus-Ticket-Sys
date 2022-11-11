8ab2f7d7e3314fe97924e71a33f5edd8cbca5ad4
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-11-11 10:50:29 +0530

    search bar added

--- Search.py
+++ Search.py
@@ -10,49 +10,17 @@ character = ImageTk.PhotoImage(character)
 L1 = Label(root, image=character, borderwidth=0)
 L1.place(x=800, y=10)
 Brand(root)
-
-
-# root window is the parent window
-fram = Frame(root)
-xx = 200
-yy = 200
-# adding label to search box
-Label(fram, text='Text to find:').pack(side=LEFT)
-Frame(root, bg="#bbf0f1", bd=400, height=250, width=500,
-      cursor="target").place(x=xx-50, y=yy-100)
-name_label = Label(root, text='Search',  fg="#105c66",
-                   bg="#bbf0f1", font=(
-                       'calibre', 14, 'bold')).place(x=xx, y=yy-50)
-# adding of single line text box
-edit = Entry(fram)
-
-# positioning of text box
-edit.pack(side=LEFT, fill=BOTH, expand=1)
-
-# setting focus
-edit.focus_set()
-
-# adding of search button
-butt = Button(fram, text='Find')
-butt.pack(side=RIGHT)
-fram.pack(side=TOP)
-
-# text box in root window
-text = Text(root)
-
-# text input area at index 1 in text window
-text.insert('1.0', '''Type your text here''')
-text.pack(side=BOTTOM)
-
+search_var = StringVar()
 
 # function to search string in text
+
+
 def find():
 
     # remove tag 'found' from index 1 to END
     text.tag_remove('found', '1.0', END)
-
     # returns to widget currently in focus
-    s = edit.get()
+    s = search_var
     if s:
         idx = '1.0'
         while 1:
@@ -72,10 +40,47 @@ def find():
 
         # mark located string as red
         text.tag_config('found', foreground='red')
-    edit.focus_set()
+    # edit.focus_set()
 
 
-butt.config(command=find)
+# root window is the parent window
+fram = Frame(root)
+xx = 200
+yy = 200
+
+
+# adding label to search box
+Frame(root, bg="#bbf0f1", bd=400, height=50, width=500,
+      cursor="target").place(x=xx-50, y=yy-50)
+name_label = Label(root, text='Search',  fg="#105c66",
+                   bg="#bbf0f1", font=(
+                       'calibre', 14, 'bold')).place(x=xx-30, y=yy-43)
+name_entry = Entry(root, textvariable=search_var, width=22, bg="#defdfd",
+                   font=('calibre', 20, 'normal')).place(x=xx+60, y=yy-43)
+
+butt = Button(root, text="🔍", command=find, fg="#105c66",
+              bg="#bbf0f1", relief=FLAT, font=(
+                  'calibre', 12, 'bold')).place(x=xx+400, y=yy-43)
+# adding of single line text box
+# edit = Entry(fram)
+
+# # positioning of text box
+# edit.pack(side=LEFT, fill=BOTH, expand=1)
+
+# # setting focus
+# edit.focus_set()
+
+# # adding of search button
+# butt = Button(fram, text='Find')
+# butt.pack(side=RIGHT)
+# fram.pack(side=TOP)
+
+# text box in root window
+text = Text(root)
+
+# text input area at index 1 in text window
+text.insert('1.0', '''Type your text here''')
+text.pack(side=BOTTOM)
 
 # mainloop function calls the endless loop of the window,
 # so the window will wait for any
