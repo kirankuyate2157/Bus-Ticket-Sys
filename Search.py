6bc5a297985b8b17231d97bfe8aff2fbfefbc25d
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-11-11 10:56:45 +0530

    search geting text

--- Search.py
+++ Search.py
@@ -16,31 +16,9 @@ search_var = StringVar()
 
 
 def find():
-
-    # remove tag 'found' from index 1 to END
-    text.tag_remove('found', '1.0', END)
-    # returns to widget currently in focus
-    s = search_var
-    if s:
-        idx = '1.0'
-        while 1:
-            # searches for desired string from index 1
-            idx = text.search(s, idx, nocase=1,
-                              stopindex=END)
-            if not idx:
-                break
-
-            # last index sum of current index and
-            # length of text
-            lastidx = '%s+%dc' % (idx, len(s))
-
-            # overwrite 'Found' at idx
-            text.tag_add('found', idx, lastidx)
-            idx = lastidx
-
-        # mark located string as red
-        text.tag_config('found', foreground='red')
-    # edit.focus_set()
+    name = search_var.get()
+    print("The search is : " + name)
+    search_var.set("")
 
 
 # root window is the parent window
@@ -61,28 +39,5 @@ name_entry = Entry(root, textvariable=search_var, width=22, bg="#defdfd",
 butt = Button(root, text="🔍", command=find, fg="#105c66",
               bg="#bbf0f1", relief=FLAT, font=(
                   'calibre', 12, 'bold')).place(x=xx+400, y=yy-43)
-# adding of single line text box
-# edit = Entry(fram)
 
-# # positioning of text box
-# edit.pack(side=LEFT, fill=BOTH, expand=1)
-
-# # setting focus
-# edit.focus_set()
-
-# # adding of search button
-# butt = Button(fram, text='Find')
-# butt.pack(side=RIGHT)
-# fram.pack(side=TOP)
-
-# text box in root window
-text = Text(root)
-
-# text input area at index 1 in text window
-text.insert('1.0', '''Type your text here''')
-text.pack(side=BOTTOM)
-
-# mainloop function calls the endless loop of the window,
-# so the window will wait for any
-# user interaction till we close it
 root.mainloop()
